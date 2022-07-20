e14af273f0ced19d983303608df9eee751ad2f4d
Md Obaidulla <mitulsunny8@gmail.com>
2022-07-20 18:58:21 -0400

    updated with Junit runner

---
+++ src/main/java/com/osa/runners/RunQA.java
@@ -0,0 +1,23 @@
+package com.osa.runners;
+
+import org.junit.runner.RunWith;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(//cucumber options properties and values
+		features = {"src/main/java/com/osa/features"},
+		glue="com.osa.steps",
+		plugin = {"pretty", "html:target/cucumber-reports" },
+		monochrome=true)
+public class RunQA {
+
+}
+
+
+//@RunWith(Cucumber.class)
+//@CucumberOptions(
+//		features = "Feature"
+//		,glue={"stepDefinition"}
+//		)
